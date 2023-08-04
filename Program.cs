6ce3ef80962a415349b2761b8492363ccd843ee0
mohdhassan7890@gmail.com <mohdhassan7890@gmail.com>
2023-08-04 22:15:50 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using Student;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student Details:");
+            Student student1 = GetStudentFromUserInput();
+            Data.Instance.AddStudent(student1);
+
+            Console.WriteLine("\nEnter teacher Details:");
+            Teacher teacher1 = GetTeacherFromUserInput();
+            Data.Instance.AddTeacher(teacher1);
+
+            Console.WriteLine("\nEnter subject Details:");
+            Subject subject1 = GetSubjectFromUserInput();
+            Data.Instance.AddSubject(subject1);
+
+            Console.WriteLine("\nEnter the class and section to display students:");
+            string classAndSectionInput = Console.ReadLine();
+            List<Student> studentsInClass = Data.Instance.GetStudentsByClassAndSection(classAndSectionInput);
+            Console.WriteLine("\nStudents in class " + classAndSectionInput + ":");
+            foreach (var student in studentsInClass)
+            {
+                Console.WriteLine($"{student.Name}");
+            }
+
+            Console.WriteLine("\nEnter the teacher name to display subjects:");
+            string teacherNameInput = Console.ReadLine();
+            List<Subject> subjectsTaughtByTeacher = Data.Instance.GetSubjectsTaughtByTeacher(teacherNameInput);
+            Console.WriteLine("\nSubjects taught by " + teacherNameInput + ":");
+            foreach (var subject in subjectsTaughtByTeacher)
+            {
+                Console.WriteLine($"{subject.Name} - {subject.SubjectCode}");
+            }
+        }
+
+        static Student GetStudentFromUserInput()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Class and Section: ");
+            string classAndSection = Console.ReadLine();
+
+            return new Student { Name = name, ClassAndSection = classAndSection };
+        }
+
+        static Teacher GetTeacherFromUserInput()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Class and Section: ");
+            string classAndSection = Console.ReadLine();
+
+            return new Teacher { Name = name, ClassAndSection = classAndSection };
+        }
+
+        static Subject GetSubjectFromUserInput()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Subject Code: ");
+            string subjectCode = Console.ReadLine();
+
+            Console.Write("Teacher Name: ");
+            string teacherName = Console.ReadLine();
+
+            return new Subject { Name = name, SubjectCode = subjectCode, Teacher = new Teacher { Name = teacherName } };
+        }
+    }
+
+}
+
